55eaab7397b5f5c94766f12ad242c94fb031d287
artsi <artsi@mail.com>
2023-05-21 23:00:45 +0300

    initial

---
+++ src/main/java/com/es/nasviazi/NasviaziApplication.java
@@ -0,0 +1,13 @@
+package com.es.nasviazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasviaziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasviaziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/es/nasviazi/NasviaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.nasviazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasviaziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
